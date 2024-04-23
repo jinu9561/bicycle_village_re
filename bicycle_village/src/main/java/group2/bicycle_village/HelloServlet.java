cc871fe65da81aa6f2a725057cd833a680aab91f
DESKTOP-4J4S7NL\admin <combihouse1233@gmail.com>
2024-04-23 11:18:36 +0900

    feat: UI, reference
    db 사용 예제 추가, header, footer 추가

--- bicycle_village/src/main/java/group2/bicycle_village/HelloServlet.java
+++ bicycle_village/src/main/java/group2/bicycle_village/HelloServlet.java
@@ -6,6 +6,8 @@ import group2.bicycle_village.dao.BoardDAOImpl;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+
+
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
--- bicycle_village/src/main/java/group2/bicycle_village/dao/BoardDAOImpl.java
+++ bicycle_village/src/main/java/group2/bicycle_village/dao/BoardDAOImpl.java
@@ -2,12 +2,23 @@ package group2.bicycle_village.dao;
 
 import group2.bicycle_village.common.util.DbUtil;
 
+import java.util.Properties;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BoardDAOImpl implements BoardDao{
+
+    public BoardDAOImpl() {
+        try {
+            System.out.println(DBQuery.boardSelect);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void test(){
         PreparedStatement ps = null;
         ResultSet rs = null;
